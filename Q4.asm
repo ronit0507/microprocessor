79883a39687446212cf2e021047e0e6a20e14444
Ronit <69254600+ronit0507@users.noreply.github.com>
2022-11-20 13:41:31 +0530

    Add files via upload

---
+++ Q4.asm
@@ -0,0 +1,35 @@
+.model tiny
+.code
+
+mov ax,02H
+mov bx,04H
+
+div bl   
+DISP PROC
+ PUSH CX
+MOV CL, 4
+MOv CH, 4
+
+D1:
+  ROL AX, CL
+  PUSH AX
+  AND AL, 0FH
+  ADD AL , 30H
+  CMP AL, '9'
+  
+  JBE D2
+  ADD AL , 7H
+  
+D2:
+  MOV AH, 02H
+  MOV DL, AL
+  INT 21H
+  POP AX 
+  DEC CH
+  JNZ D1
+  POP CX
+  RET
+  DISP ENDP
+  
+.exit
+End
